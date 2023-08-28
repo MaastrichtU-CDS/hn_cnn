f1bc573301b841b5b78147864206f67b8380ff4f
Pedro Mateus <pedro.mateus@maastro.nl>
2023-08-28 12:15:20 +0200

    Added option to store the model

--- hn_cnn/fit.py
+++ hn_cnn/fit.py
@@ -2,7 +2,7 @@ import torch
 
 from hn_cnn.cnn import HNLR
 from hn_cnn.constants import *
-from hn_cnn.utils import update_parameters
+from hn_cnn.utils import update_parameters, save_model
 
 DEFAULT_HYPERPARAMETERS = {
     EPOCHS: 3000,
@@ -23,10 +23,11 @@ def evaluate(model, val_loader, weights):
         metrics = model.validation_step(batch, weights)
     return metrics
 
-def fit(model, data_loaders, parameters={}):
+def fit(model, data_loaders, parameters={}, store_model={}):
     """ Train the model.
     """
     output = []
+    best_val_auc = 0
     hyperparameters = update_parameters(parameters, DEFAULT_HYPERPARAMETERS)
     if isinstance(model, HNLR):
         # Train the logistic regression model
@@ -57,11 +58,6 @@ def fit(model, data_loaders, parameters={}):
         # start_time = time.time()
         for epoch in range(0, hyperparameters[EPOCHS]):
             print(f"Epoch {epoch}/{hyperparameters[EPOCHS]}")
-            metrics = {}
-            for subset, data_loader in data_loaders.items():
-                if subset != TRAIN:
-                    subset_metrics = evaluate(model, data_loader, hyperparameters[CLASS_WEIGHTS])
-                    metrics[subset] = subset_metrics
             # Training
             model.train()
             for batch in data_loaders[TRAIN]:
@@ -98,8 +94,20 @@ def fit(model, data_loaders, parameters={}):
                         print(subset)
                         print(subset_metrics)
                 output.append(metrics)
-
-                if metrics[TRAIN_METRICS][ROC][AUC] > 0.95:
+                if store_model[MODEL_PATH] and metrics[VALIDATION][ROC][AUC] > best_val_auc \
+                    and metrics[VALIDATION][ROC][AUC] > store_model.get(THRESHOLD, 0) \
+                        and abs(metrics[VALIDATION][ROC][AUC] - metrics[VALIDATION][ROC][AUC]) < \
+                            store_model.get(MAX_DIFFERENCE, 1):
+                    save_model(
+                        store_model[MODEL_PATH],
+                        epoch,
+                        model,
+                        optimizer,
+                        loss,
+                        model_id=store_model[MODEL_ID] or str(type(model))
+                    )
+                    best_val_auc = metrics[VALIDATION][ROC][AUC]
+                if metrics[TRAIN_METRICS][ROC][AUC] > store_model.get(STOP_THRESHOLD, 0.95):
                     break 
 
     return output
--- hn_cnn/utils.py
+++ hn_cnn/utils.py
@@ -1,3 +1,5 @@
+import torch
+
 def get_parameter(parameter, parameters, default_parameters):
     """ Get parameter from the environmnet variables, otherwise
         use the default value provided.
@@ -13,3 +15,13 @@ def update_parameters(parameters, default_parameters):
     for parameter, value in parameters.items():
         update_parameters[parameter] = value
     return update_parameters
+
+def save_model(path, epoch, model, optimizer, loss, model_id="0"):
+    """ Store the model.
+    """
+    torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': loss,
+    }, f'{path}/model_{model_id}_{epoch}.pth.tar')
