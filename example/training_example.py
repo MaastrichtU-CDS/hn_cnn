0fe981a474a77e56daf3dd8f3bf51607f5bb6f55
Pedro Mateus <pedro.mateus@maastro.nl>
2023-09-20 20:06:21 +0200

    Refactor example

--- example/training_example.py
+++ example/training_example.py
@@ -14,6 +14,50 @@ from  hn_cnn.data_augmentation import get_training_augmentation, TRANSFORM_TO_TE
 from hn_cnn.fit import fit
 from hn_cnn.parse_data import ImageDataset
 
+def run_network(folders, config, ids):
+    """ Run the network according to the configuration provided
+        - folders: paths to the folders for training, validation, and testing
+        - config: configuration regarding the network to train, data split, ...
+        - ids: ids for each subset
+    """
+    dataloaders = {}
+    # Load datasets
+    # Include a training dataset that won't be augmented in order to
+    # calculate the performance metrics
+    folders[TRAIN_METRICS] = folders[TRAIN]
+    is_neural_network = config[MODEL] in [CNN, ANN]
+    for dataset in [TRAIN, TRAIN_METRICS, VALIDATION, TESTING]:
+        paths = DATA[dataset]
+        dataset_ids = ids[dataset]
+        is_training = (dataset == TRAIN)
+        dataset_parsed = ImageDataset(
+            paths[CLINICAL_DATA_PATH],
+            paths[SCANS_PATH],
+            get_training_augmentation(augment=config[DATA_AUGMENTATION]) if is_training else TRANSFORM_TO_TENSOR,
+            ids_to_use=dataset_ids,
+            timeframe=config[TIME_TO_EVENT],
+            event=config[EVENT],
+        )
+        dataloaders[dataset] = DataLoader(
+            dataset_parsed,
+            batch_size = config[BATCH_SIZE] if is_training and is_neural_network else len(dataset_parsed.images),
+            shuffle = is_training,
+            drop_last = is_training,
+        )
+
+    model = MODELS[config[MODEL]](
+        clinical_data=config.get(CLINICAL_VARIABLES),
+    )
+    print(model)
+    # Print a summary of the model
+    # summary(model, (1, 180, 180))
+    history = fit(
+        model,
+        dataloaders,
+        parameters=config[HYPERPARAMETERS],
+    )
+    return history
+
 DATA = {
     TRAIN: {
         CLINICAL_DATA_PATH: os.getenv(f"{TRAIN}_{CLINICAL_DATA_PATH}"),
@@ -31,7 +75,7 @@ DATA = {
 
 CONFIGURATIONS = {
     MODEL: LR,
-    DATA_SPLIT: CROSS_VALIDATION,
+    DATA_SPLIT: COHORT_SPLIT,
     FOLDS: 2,
     TIME_TO_EVENT: 2 * 365,
     EVENT: DM,
@@ -77,42 +121,16 @@ if CONFIGURATIONS[DATA_SPLIT] == COHORT_SPLIT:
     dataloaders = {}
     train_ids = [id for id in dataset_train.keys if "HGJ" in id or "CHUS" in id]
     validation_ids =  [id for id in dataset_train.keys if "HMR" in id or "CHUM" in id]
-    # Load datasets
-    # Include a training dataset that won't be augmented in order to
-    # calculate the performance metrics
+
     DATA[TRAIN_METRICS] = DATA[TRAIN]
-    is_neural_network = CONFIGURATIONS[MODEL] in [CNN, ANN]
+    dataset_ids = {}
     for dataset in [TRAIN, TRAIN_METRICS, VALIDATION, TESTING]:
-        paths = DATA[dataset]
-        dataset_ids = []
         is_training = (dataset == TRAIN)
+        dataset_ids[dataset] = []
         if dataset in [TRAIN, TRAIN_METRICS, VALIDATION]:
-            dataset_ids = train_ids if is_training or dataset == TRAIN_METRICS else validation_ids
-        dataset_parsed = ImageDataset(
-            paths[CLINICAL_DATA_PATH],
-            paths[SCANS_PATH],
-            get_training_augmentation(augment=CONFIGURATIONS[DATA_AUGMENTATION]) if is_training else TRANSFORM_TO_TENSOR,
-            ids_to_use=dataset_ids,
-            timeframe=CONFIGURATIONS[TIME_TO_EVENT],
-            event=CONFIGURATIONS[EVENT],
-        )
-        dataloaders[dataset] = DataLoader(
-            dataset_parsed,
-            batch_size = CONFIGURATIONS[BATCH_SIZE] if is_training and is_neural_network else len(dataset_parsed.images),
-            shuffle = is_training,
-            drop_last = is_training,
-        )
-    model = MODELS[CONFIGURATIONS[MODEL]](
-        clinical_data=CONFIGURATIONS.get(CLINICAL_VARIABLES),
-    )
-    print(model)
-    # Print a summary of the model
-    # summary(NaturalSceneClassification(), (1, 180, 180))
-    history = fit(
-        model,
-        dataloaders,
-        parameters=CONFIGURATIONS[HYPERPARAMETERS],
-    )
+            dataset_ids[dataset] = train_ids if is_training or dataset == TRAIN_METRICS else validation_ids
+    run_network(DATA, CONFIGURATIONS, dataset_ids)
+
 elif CONFIGURATIONS[DATA_SPLIT] == CROSS_VALIDATION:
     kfold = StratifiedKFold(n_splits=CONFIGURATIONS[FOLDS], shuffle=True, random_state=random_seed_split)
     dataset_train = ImageDataset(
@@ -123,47 +141,17 @@ elif CONFIGURATIONS[DATA_SPLIT] == CROSS_VALIDATION:
         event=CONFIGURATIONS[EVENT],
     )
     for fold, (train_ids, validation_ids) in enumerate(kfold.split(dataset_train, np.array(dataset_train.y))):
-            print(f"Fold: {fold}")
-            # Store the model training logs in a file
-            file_path = CONFIGURATIONS[LOGS_PATH]
-            #with open(file_path, "w") as o:
-            #    with contextlib.redirect_stdout(o):
-            dataloaders = {}
-            # Load datasets
-            # Include a training dataset that won't be augmented in order to
-            # calculate the performance metrics
-            DATA[TRAIN_METRICS] = DATA[TRAIN]
-            is_neural_network = CONFIGURATIONS[MODEL] in [CNN, ANN]
-            for dataset in [TRAIN, TRAIN_METRICS, VALIDATION, TESTING]:
-                paths = DATA[dataset]
-                dataset_ids = []
-                is_training = (dataset == TRAIN)
-                if dataset in [TRAIN, TRAIN_METRICS, VALIDATION]:
-                    dataset_ids = [dataset_train.keys[i] for i in train_ids] if \
-                        is_training or dataset == TRAIN_METRICS else [dataset_train.keys[i] for i in validation_ids]
-                dataset_parsed = ImageDataset(
-                    paths[CLINICAL_DATA_PATH],
-                    paths[SCANS_PATH],
-                    get_training_augmentation(augment=CONFIGURATIONS[DATA_AUGMENTATION]) if is_training else TRANSFORM_TO_TENSOR,
-                    ids_to_use=dataset_ids,
-                    timeframe=CONFIGURATIONS[TIME_TO_EVENT],
-                    event=CONFIGURATIONS[EVENT],
-                )
-                dataloaders[dataset] = DataLoader(
-                    dataset_parsed,
-                    batch_size = CONFIGURATIONS[BATCH_SIZE] if is_training and is_neural_network else len(dataset_parsed.images),
-                    shuffle = is_training,
-                    drop_last = is_training,
-                )
-
-            model = MODELS[CONFIGURATIONS[MODEL]](
-                clinical_data=CONFIGURATIONS.get(CLINICAL_VARIABLES),
-            )
-            print(model)
-            # Print a summary of the model
-            # summary(NaturalSceneClassification(), (1, 180, 180))
-            history = fit(
-                model,
-                dataloaders,
-                parameters=CONFIGURATIONS[HYPERPARAMETERS],
-            )
+        print(f"Fold: {fold}")
+        # Store the model training logs in a file
+        file_path = CONFIGURATIONS[LOGS_PATH]
+        #with open(file_path, "w") as o:
+        #    with contextlib.redirect_stdout(o):
+        DATA[TRAIN_METRICS] = DATA[TRAIN]
+        dataset_ids = {}
+        for dataset in [TRAIN, TRAIN_METRICS, VALIDATION, TESTING]:                
+            is_training = (dataset == TRAIN)
+            dataset_ids[dataset] = []
+            if dataset in [TRAIN, TRAIN_METRICS, VALIDATION]:
+                dataset_ids[dataset] = [dataset_train.keys[i] for i in train_ids] if \
+                    is_training or dataset == TRAIN_METRICS else [dataset_train.keys[i] for i in validation_ids]
+        run_network(DATA, CONFIGURATIONS, dataset_ids)
